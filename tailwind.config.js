7804f9c22f99e34b39f763f809d6567b328e8089
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-24 11:05:01 +0200

    Added primeng and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations';
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animations],
+}
+
