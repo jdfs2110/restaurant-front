d676dd590a9597c051c412054e166d8f4f6b2a53
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-24 12:56:20 +0200

    Add LoginForm and RegisterForm types

---
+++ src/app/types/RegisterForm.ts
@@ -0,0 +1,9 @@
+export type RegisterForm = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+  id_rol: number
+}
+
+// Tengo que pensar qué hacer sobre los roles, si dejar input por usuario o hacerlo manual
