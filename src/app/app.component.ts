0128e8fefa8b328dbb5d7320b502368afaa3133a
jdfs2110 <jdfsiescamp@gmail.com>
2024-05-06 15:01:39 +0200

    feat: Add ngx-translate for spanish support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { PrimeNGConfig } from 'primeng/api';
+import json from '@/assets/i18n/es.json'
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +16,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'restaurant-front';
+
+  constructor(private config: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.config.setTranslation(json)
+  }
 }
